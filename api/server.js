6c31f6da0937e01d67f4c5d7d4646b15df476062
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-04-03 18:15:53 +0530

    connect to database

--- api/server.js
+++ api/server.js
@@ -1,6 +1,21 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = express();
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+.then(() => {
+    console.log('Connected to MongoDB');
+}
+)
+.catch((error) => {
+    console.log('Error connecting database.');
+}
+);
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
