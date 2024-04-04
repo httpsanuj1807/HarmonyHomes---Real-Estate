5a0cddddaa7f96e831c01b882e840d054c7d3cb9
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-04-04 12:25:43 +0530

    create a middleware and a function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
 
+
 const saltRounds = 10;
 
-export const signup = async(req, res) => {
+export const signup = async(req, res, next) => {
     const { username, email, password } = req.body; 
     const hashed = bcrypt.hashSync(password, saltRounds);
     const newUser = new User({username, email, password : hashed});
@@ -12,6 +13,6 @@ export const signup = async(req, res) => {
         res.status(201).json("User created successfully.");
     }
     catch(err){
-        res.status(500).json(err.message);
+        next(err);
     }
 }
--- api/server.js
+++ api/server.js
@@ -28,9 +28,20 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // routes middlewares
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode, 
+        message
+    });
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
