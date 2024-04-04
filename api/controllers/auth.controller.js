6cfdd22486cab598756723f42255db6f9fb32d28
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-04-04 22:41:57 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
+import { errorHandler } from "../utils/error.js";
 
 const saltRounds = 10; 
 
@@ -15,4 +16,28 @@ export const signup = async(req, res, next) => {
     catch(err){
         next(err);
     }
-}
+};
+
+
+export const signin = async(req, res, next) =>{
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'User not found'));
+        }
+        const isValidPassword = bcrypt.compareSync(password, validUser.password);
+        if(!isValidPassword){
+            return next(errorHandler(401,'Invalid email or password.'));
+        }
+        const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET);
+        const {password : pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {
+            httpOnly : true,
+            maxAge : 1000 * 60 * 60 * 24,
+        }).status(200).json(rest);
+    }
+    catch(err){
+        next(err);
+    }
+};
