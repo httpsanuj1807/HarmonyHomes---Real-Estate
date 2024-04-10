bc9420a33703d12ee7092ecde9577ba8c41501d3
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-04-10 20:49:23 +0530

    create update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,36 +1,59 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createListing = async(req, res, next) => {
-    try{
-        const listing = await Listing.create(req.body);
-        res.status(201).json(listing);
-    }   
-    catch(err){
-        next(err);
-    }
-}
-
-
-export const deleteListing = async(req, res, next) => {
-    try{
-        const listing = await Listing.findById(req.params.id);
-        if(!listing){
-            return next(errorHandler(404, 'No listing found.'));
-        }
-        if(listing.userRef != req.user.id){
-            return next(errorHandler(401, 'You are only allowed to delete your own listings.'))
-        }
-        try{
-            await Listing.findByIdAndDelete(req.params.id);
-            res.status(200).json('Listing deleted successfully');
-        }
-        catch(err){
-            next(err);
-        }
-    }   
-    catch(err){
-        next(err);
-    }
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    res.status(201).json(listing);
+  } catch (err) {
+    next(err);
+  }
 };
 
+export const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "No listing found."));
+    }
+    if (listing.userRef != req.user.id) {
+      return next(
+        errorHandler(401, "You are only allowed to delete your own listings.")
+      );
+    }
+    try {
+      await Listing.findByIdAndDelete(req.params.id);
+      res.status(200).json("Listing deleted successfully");
+    } catch (err) {
+      next(err);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "No listing found."));
+    }
+    if (listing.userRef != req.user.id) {
+      return next(
+        errorHandler(401, "You are only allowed to update your own listings.")
+      ); 
+    }
+    try {
+      const updatedListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.status(200).json(updatedListing);
+    } catch (err) {
+      next(err);
+    }
+  } catch (err) {
+    next(err.message);
+  }
+};
