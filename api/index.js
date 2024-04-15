2ba29e4dff3624e483b598cfc38065109fc81e91
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-04-15 16:05:05 +0530

    updated pre installation

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'; 
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 const app = express();
 
@@ -25,6 +25,8 @@ await mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 }
 );
 
+const __dirname = path.resolve();
+
 
 
 
@@ -37,6 +39,13 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 
 // error handling middleware
 app.use((err,req,res,next)=>{
