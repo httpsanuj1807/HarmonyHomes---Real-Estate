3515ad684789bcc8a3fe9b170d86e57f51bdbca9
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-04-15 14:03:57 +0530

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://localhost:3000',
-        secure: false
-      }
-    }
-  },
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Apply proxy configuration only during development
+      ...(process.env.NODE_ENV === 'development' && {
+        '/api': {
+          target: 'http://localhost:3000',
+          secure: false
+        }
+      })
+    }
+  }
+});
